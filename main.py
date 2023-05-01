7b6deb8d6f14fa1049d35c04abce8c50bb10a5d1
tania <123573477+totioti@users.noreply.github.com>
2023-05-01 23:05:53 +0300

    creating a hello_test() function

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from PyQt5.QtWidgets import QApplication
 from Source.window import Window
 
 
+def hello_test():
+    print('Hello EquTest')
+
+
 def main():
     application = QApplication(sys.argv)
     window = Window()
 
     window.show()
     application.exec_()
-    
+
 
 if __name__ == "__main__":
     main()
+
