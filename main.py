737b842abd6c1fa8a00e558939cc6ba6b0b51938
Володя <102909208+eesuck1@users.noreply.github.com>
2023-05-01 23:11:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from PyQt5.QtWidgets import QApplication
 from Source.window import Window
 
 
-def hello_test():
-    print('Hello EquTest')
-
-
 def main():
     application = QApplication(sys.argv)
     window = Window()
