a9e295c7261f3fb2b301de7c5c25d0f08ca14503
eesuck1 <eesucksc@gmail.com>
2023-05-10 15:27:30 +0300

    Added the UI

--- Source/window.py
+++ Source/window.py
@@ -2,7 +2,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 
 from Source.singleton import singleton
-from UI.Menu.menu_ui import UiMainWindow
+from UI.Menu.menu_ui import MenuUI
+from UI.Window.window_ui import WindowUI
+from UI.RegisterWindow.register_window_ui import RegisterUI
 from Database.database import Database
 from Source.constants import PROFESSOR_TYPE, STUDENT_TYPE
 
@@ -13,12 +15,11 @@ class Window(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.__database__ = Database()
-
-        self.__init_UI__()
+        self.__ui__ = WindowUI()
 
     def __init_UI__(self, *args, **kwargs) -> None:
         """Add the implementation"""
-        ...
+        self.__ui__.setupUi(self)
 
     def _switch_windows_(self, another_window: QtWidgets.QMainWindow, hide: bool = True) -> None:
         """Hide a current window and replace it by a caller"""
@@ -36,9 +37,10 @@ class Window(QtWidgets.QMainWindow):
 @singleton
 class Menu(Window):
     def __init__(self):
-        self.__ui__ = UiMainWindow()
-
         super().__init__()
+
+        self.__ui__ = MenuUI()
+        self.__init_UI__()
 
     def __init_UI__(self) -> None:
         """Implementation for Menu Class"""
@@ -48,7 +50,7 @@ class Menu(Window):
         self.__dialogue__ = RegisterWindow()
 
         self.__ui__.__button__.clicked.connect(self._check_enter_data_)
-        
+
     def _check_enter_data_(self) -> None:
         is_student = self._check_user_(STUDENT_TYPE)
         is_professor = self._check_user_(PROFESSOR_TYPE)
@@ -74,15 +76,17 @@ class RegisterWindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
 
+        self.__ui__ = RegisterUI()
         self.__init_UI__()
 
     def __init_UI__(self) -> None:
         """Add the UI"""
-        ...
+        self.__ui__.setupUi(self)
 
     def _add_user_(self) -> None:
         """Adding the user to database"""
         ...
+
 
 @singleton
 class StudentWindow(Window):
--- UI/Menu/menu_ui.py
+++ UI/Menu/menu_ui.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class UiMainWindow(object):
+class MenuUI(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1280, 720)
